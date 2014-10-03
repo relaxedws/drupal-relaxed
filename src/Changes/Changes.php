35d6a8d18c412f5bfe39a34c7c8ad7a8f39cd205
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-10-03 15:37:38 +0200

    Use last revision instead of all revisions for each sequence.

--- src/Changes/Changes.php
+++ src/Changes/Changes.php
@@ -38,25 +38,8 @@ class Changes implements ChangesInterface {
    */
   public function getNormal() {
     $changes = $this->sequenceIndex
-      ->useWorkspace($this->workspaceName);
-
-    $changes = $this->sequenceIndex
       ->useWorkspace($this->workspaceName)
       ->getRange($this->lastSeq, NULL);
-
-    $id = 0;
-    $entities = array();
-    // Create an array with entities.
-    foreach ($changes as $key => $change) {
-      if ($id == 0) {
-        $id = $change['entity_id'];
-      }
-      elseif ($change['entity_id'] == $id) {
-        continue;
-      }
-      $id = $change['entity_id'];
-      $entities[$id] = entity_load($change['entity_type'], $id);
-    }
 
     $result = array();
     $last_seq_number = count($changes);
@@ -65,31 +48,14 @@ class Changes implements ChangesInterface {
       $result['last_seq'] = $changes[$last_seq_number]['local_seq'];
     }
 
-
     // Format the result array.
     foreach ($changes as $change) {
-      $entity = $entities[$change['entity_id']];
-      $id = $entity->uuid();
-
-      $revs = array();
-      $revs_count = $entity->_revs_info->count();
-      if ($revs_count > 0) {
-        $rev_number = 1;
-        $id = $entity->uuid();
-        while ($rev_number <= $revs_count) {
-          if ($rev = $entity->_revs_info->get($rev_number)->rev) {
-            $revs[] = array(
-              'rev' => $rev,
-            );
-          }
-          $rev_number++;
-        }
-      }
-
       $result['results'][] = array(
-        'changes' => $revs,
+        'changes' => array(
+          'rev' => $change['rev'],
+        ),
         'deleted' => $change['deleted'],
-        'id' => $id,
+        'id' => $change['entity_id'],
         'seq' => $change['local_seq'],
       );
     }
