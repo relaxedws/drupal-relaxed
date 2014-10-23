d3b07586eb11dfbaf449b6c6437e34586b8b807a
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-10-23 21:05:09 +0300

    Initial resource to return all entity types in the system.

---
+++ src/Plugin/rest/resource/AllDbResource.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\relaxed\Plugin\rest\resource\AllDbResource.
+ */
+
+namespace Drupal\relaxed\Plugin\rest\resource;
+
+use Drupal\Core\Entity\EntityStorageException;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * @file
+ * Implements http://docs.couchdb.org/en/latest/api/server/common.html#all-dbs
+ */
+
+/**
+ * @RestResource(
+ *   id = "relaxed:all_dbs",
+ *   label = "All Workspaces",
+ *   serialization_class = {
+ *     "canonical" = "Drupal\multiversion\Entity\WorkspaceInterface",
+ *     "post" = "Drupal\Core\Entity\ContentEntityInterface",
+ *   },
+ *   uri_paths = {
+ *     "canonical" = "/_all_dbs",
+ *   }
+ * )
+ */
+class AllDbResource extends ResourceBase {
+
+  public function get() {
+    $entity_manager = \Drupal::entityManager();
+    $entity_definitions = $entity_manager->getDefinitions();
+    $result = array_keys($entity_definitions);
+
+    return new ResourceResponse($result, 200);
+  }
+}
