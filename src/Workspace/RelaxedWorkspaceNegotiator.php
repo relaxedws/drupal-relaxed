7b9e796e7de753cd8c205a262805edc12c302a72
Dick Olsson <dick.olsson@senzilla.com>
2014-10-28 21:48:28 +0000

    Added a Relaxed workspace negotiator

---
+++ src/Workspace/RelaxedWorkspaceNegotiator.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\relaxed\Workspace;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\multiversion\Workspace\WorkspaceNegotiatorBase;
+use Symfony\Component\HttpFoundation\Request;
+
+class RelaxedWorkspaceNegotiator extends WorkspaceNegotiatorBase {
+
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(Request $request) {
+    $api_root = trim($this->configFactory->get('relaxed.settings')->get('api_root'), '/');
+    $path_info = trim($request->getPathInfo(), '/');
+
+    if (strpos($path_info, $api_root) === 0) {
+      $paths = explode('/', $path_info);
+      // If we have more than one part, and the second part is not an internal
+      // resource, then it means the second part is the workspace ID.
+      if (count($paths) > 1 && substr($paths[1], 0, 1) != '_') {
+        return TRUE;
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getWorkspaceId(Request $request) {
+    $path_info = trim($request->getPathInfo(), '/');
+    $paths = explode('/', $path_info);
+    return $paths[1];
+  }
+
+}
