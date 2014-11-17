87f8b2056c7b77028a8455d8f37a81d824295564
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-11-17 15:58:18 +0200

    Implement the session endpoint and GET method.

---
+++ src/Plugin/rest/resource/SessionResource.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\relaxed\Plugin\rest\resource\SessionResource.
+ */
+
+namespace Drupal\relaxed\Plugin\rest\resource;
+
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+/**
+ * @RestResource(
+ *   id = "relaxed:session",
+ *   label = "Session",
+ *   uri_paths = {
+ *     "canonical" = "/_session",
+ *   }
+ * )
+ */
+class SessionResource extends ResourceBase {
+
+  /**
+   * @return ResourceResponse
+   */
+  public function get() {
+    $account = \Drupal::currentUser();
+    if ($account->isAnonymous()) {
+      throw new UnauthorizedHttpException('', 'Username or password was not recognized.');
+    }
+
+    $roles = array_values($account->getRoles());
+    $admin_role = \Drupal::config('user.settings')->get('admin_role');
+    if (in_array($admin_role, $roles)) {
+      $roles[] = '_admin';
+    }
+
+    return new ResourceResponse(
+      array(
+        'info' => array(),
+        'ok' => TRUE,
+        'userCtx' => array(
+          'user' => $account->getUsername(),
+          'roles' => $roles,
+        ),
+      ),
+      200
+    );
+  }
+}
---
+++ src/Tests/SessionResourceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\relaxed\Tests;
+
+use Drupal\Component\Serialization\Json;
+
+/**
+ * Tests the /_session resource.
+ *
+ * @group relaxed
+ */
+class SessionResourceTest extends ResourceTestBase {
+
+  public function testGet() {
+    $this->enableService('relaxed:session', 'GET');
+
+    // Create a user with the correct permissions.
+    $permissions = array('restful get relaxed:session', 'administer permissions', 'administer users');
+    $account = $this->drupalCreateUser($permissions, 'test_user');
+    $roles = $account->getRoles();
+    $this->drupalLogin($account);
+    \Drupal::config('user.settings')->set('admin_role', $roles[0])->save();
+
+    $response = $this->httpRequest('_session', 'GET', NULL);
+    $this->assertResponse('200', 'HTTP response code is correct.');
+    $this->assertHeader('content-type', $this->defaultMimeType);
+    $data = Json::decode($response);
+
+    $roles = array(
+      'authenticated',
+      $roles[0],
+      '_admin',
+    );
+    $expected = array(
+      'info' => array(),
+      'ok' => TRUE,
+      'userCtx' => array(
+        'user' => 'test_user',
+        'roles' => $roles,
+      ),
+    );
+    $this->assertIdentical($expected, $data, ('Correct values in response.'));
+  }
+}
