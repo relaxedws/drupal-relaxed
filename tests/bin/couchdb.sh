7b4fc1369b88eac479d22ec78ec18dff7609d79c
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-11-27 13:53:38 +0200

    Travis script fixes.

--- tests/bin/couchdb.sh
+++ tests/bin/couchdb.sh
@@ -9,8 +9,8 @@ drush en --yes relaxed_test || true
 # Create a target database and do the replication.
 curl -X PUT localhost:5984/target
 curl -X GET http://admin:admin@localhost/relaxed/default
-nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://admin:admin@localhost/relaxed/default", "target": "http://localhost:5984/target"}' http://localhost:5984/_replicate
-sleep 10
+nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://admin:admin@localhost/relaxed/default", "target": "http://localhost:5984/target"}' http://localhost:5984/_replicate &
+sleep 300
 
 # Output information useful for debugging.
 sudo cat /var/log/couchdb/couch.log
