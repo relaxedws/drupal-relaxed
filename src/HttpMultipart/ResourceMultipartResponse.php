fb12222a9b197018993b4a6e4f497ede3fcb80a9
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-12-11 16:13:03 +0200

    Fix the timeout error on replication.

--- src/HttpMultipart/ResourceMultipartResponse.php
+++ src/HttpMultipart/ResourceMultipartResponse.php
@@ -8,9 +8,22 @@
 namespace Drupal\relaxed\HttpMultipart;
 
 use Drupal\relaxed\HttpMultipart\HttpFoundation\MultipartResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Contains data for serialization before sending the response.
  */
-class ResourceMultipartResponse extends MultipartResponse { }
+class ResourceMultipartResponse extends MultipartResponse {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepare(Request $request)
+  {
+    // Fix the timeout error on replication.
+    $this->headers->set('Connection', 'close');
+
+    return parent::prepare($request);
+  }
+}
--- tests/bin/drupal-to-couchdb.sh
+++ tests/bin/drupal-to-couchdb.sh
@@ -13,15 +13,15 @@ do
   curl -X POST \
        -H "Content-Type: application/json" \
        -d "$document" \
-       admin:admin@localhost/relaxed/default
-done < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt
+       admin:andre1@drupal8dev.loc/relaxed/default
+done < tests/fixtures/documents.txt
 
 # Create a target database and do the replication.
 curl -X PUT localhost:5984/target
 
 # Run the replication.
 nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://admin:admin@localhost/relaxed/default", "target": "http://localhost:5984/target"}' http://localhost:5984/_replicate &
-sleep 180
+sleep 120
 
 curl -X GET http://localhost:5984/target/_all_docs | tee /tmp/all_docs.txt
 
