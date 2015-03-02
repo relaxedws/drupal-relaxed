69a1e48af854c9aaa99001902dc952a1df90405f
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-03-02 18:41:29 +0200

    Use one worker for drupal-to-couchdb replication.

--- tests/bin/drupal-to-drupal.sh
+++ tests/bin/drupal-to-drupal.sh
@@ -29,7 +29,7 @@ drush cr
 
 # Run the replication.
 nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://admin:admin@drupal.loc/relaxed/default", "target": "http://admin:admin@drupal2.loc/relaxed/default", "worker_processes": 1}' http://localhost:5984/_replicate &
-sleep 200
+sleep 120
 
 curl -X GET http://admin:admin@drupal2.loc/relaxed/default/_all_docs | tee /tmp/all_docs.txt
 
