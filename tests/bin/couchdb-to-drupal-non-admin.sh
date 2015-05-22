5f8683e4778982694a8d7ca2b1b1dd22f20bc2e3
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-05-22 11:12:23 +0300

    Show error and access logs.

--- tests/bin/couchdb-to-drupal-non-admin.sh
+++ tests/bin/couchdb-to-drupal-non-admin.sh
@@ -38,6 +38,14 @@ sleep 120
 
 curl -X GET http://admin:admin@drupal.loc/relaxed/default/_all_docs | tee /tmp/all_docs.txt
 
+#-----------------------------------
+sudo cat /var/log/couchdb/couch.log
+#-----------------------------------
+sudo cat /var/log/apache2/error.log
+#-----------------------------------
+sudo cat /var/log/apache2/forensic.log
+#-----------------------------------
+
 COUNT=$(wc -l < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt)
 USERS=3
 COUNT=$(($COUNT + $USERS));
