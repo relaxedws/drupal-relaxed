9433632fddfaeff0828106f27fd26ab1a819d9b2
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-05-22 11:12:23 +0300

    Show error and access logs.

--- tests/bin/drupal-to-couchdb.sh
+++ tests/bin/drupal-to-couchdb.sh
@@ -26,6 +26,14 @@ sleep 120
 
 curl -X GET http://localhost:5984/target/_all_docs | tee /tmp/all_docs.txt
 
+#-----------------------------------
+sudo cat /var/log/couchdb/couch.log
+#-----------------------------------
+sudo cat /var/log/apache2/error.log
+#-----------------------------------
+sudo cat /var/log/apache2/forensic.log
+#-----------------------------------
+
 COUNT=$(wc -l < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt)
 USERS=2
 COUNT=$(($COUNT + $USERS));
