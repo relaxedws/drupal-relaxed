9763038e4c640b2d21c8a1ab10e5c1cf058e1bdc
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-06-10 12:30:17 +0300

    Save and update stub entities in DbResource and configure fixtures.

--- src/Plugin/rest/resource/DbResource.php
+++ src/Plugin/rest/resource/DbResource.php
@@ -4,6 +4,7 @@ namespace Drupal\relaxed\Plugin\rest\resource;
 
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityStorageException;
+use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\multiversion\Entity\WorkspaceInterface;
 use Drupal\rest\ResourceResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
@@ -110,12 +111,12 @@ class DbResource extends ResourceBase {
     $rev = $entity->_rev->value;
 
     // Check for conflicts.
-    if ($uuid) {
+    /*if ($uuid) {
       $entry = \Drupal::service('entity.index.uuid')->get($uuid);
       if (!empty($entry)) {
         throw new ConflictHttpException();
       }
-    }
+    }*/
 
     // Check entity and field level access.
     if (!$entity->access('create')) {
@@ -125,6 +126,48 @@ class DbResource extends ResourceBase {
       if (!$field->access('create')) {
         throw new AccessDeniedHttpException(t('Access denied on creating field @field.', array('@field' => $field_name)));
       }
+
+      // For entity reference fields we should check if the referenced entity
+      // exists or we should save a stub entity.
+      if ($field instanceof EntityReferenceFieldItemListInterface) {
+        foreach ($field as $delta => $item) {
+          // Create a stub entity for entity reference field if
+          // it doesn't exist.
+          if (isset($item->entity_to_save)) {
+            $entity_to_save = $item->entity_to_save;
+            $existent_entities = entity_load_multiple_by_properties(
+              $item->entity_to_save->getEntityTypeId(),
+              array('uuid' => $item->entity_to_save->uuid())
+            );
+            $existent_entity = reset($existent_entities);
+            // Unset information about the entity_to_save.
+            unset($entity->{$field_name}[$delta]->entity_to_save);
+            // If the entity already exists, don't save the stub entity, just
+            // complete the field with the correct target_id.
+            if ($existent_entity) {
+              $entity->{$field_name}[$delta] = array('target_id' => $existent_entity->id());
+              continue;
+            }
+            // Save the stub entity and set the target_id value to the field item.
+            $entity_to_save->save();
+            $entity->{$field_name}[$delta] = array('target_id' => $entity_to_save->id());
+          }
+        }
+      }
+    }
+
+    $existent_entities = entity_load_multiple_by_properties($entity->getEntityTypeId(), array('uuid' => $entity->uuid()));
+    $existent_entity = reset($existent_entities);
+    // Update a stub entity with the correct values.
+    if ($existent_entity && !$entity->id()) {
+      $id_key = $entity->getEntityType()->getKey('id');
+      foreach ($existent_entity as $field_name => $field) {
+        if ($field_name != $id_key && $entity->{$field_name}->value) {
+          $existent_entity->{$field_name}->value = $entity->{$field_name}->value;
+        }
+      }
+      $entity = $existent_entity;
+      $entity->isDefaultRevision(TRUE);
     }
 
     // Validate the received data before saving.
