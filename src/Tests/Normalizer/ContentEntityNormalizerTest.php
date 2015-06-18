c93184f9cec5335c0dcccb575e2fa96efd99bf91
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-06-18 15:37:37 +0300

    Work with Drupal 8 dev branch.

--- src/Tests/Normalizer/ContentEntityNormalizerTest.php
+++ src/Tests/Normalizer/ContentEntityNormalizerTest.php
@@ -16,7 +16,7 @@ use Drupal\Component\Utility\SafeMarkup;
  */
 class ContentEntityNormalizerTest extends NormalizerTestBase {
 
-  public static $modules = array('serialization', 'system', 'entity', 'field', 'entity_test', 'text', 'filter', 'user', 'key_value', 'multiversion', 'rest', 'uuid', 'relaxed');
+  public static $modules = array('serialization', 'system', 'field', 'entity_test', 'text', 'filter', 'user', 'key_value', 'multiversion', 'rest', 'relaxed');
 
   protected $entityClass = 'Drupal\entity_test\Entity\EntityTest';
 
