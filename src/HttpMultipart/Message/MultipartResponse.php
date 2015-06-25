ebe8b9d2c028a8110990c39510ad56f860593ed0
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-06-25 16:10:36 +0300

    Add comments and code refactoring.

--- src/HttpMultipart/Message/MultipartResponse.php
+++ src/HttpMultipart/Message/MultipartResponse.php
@@ -58,8 +58,12 @@ class MultipartResponse extends Response
         }
         $headers = [];
         $message_parts = explode("\r\n\r\n", $message, 2);
+        // In $message_parts we should have two values - headers ($message_parts[0])
+        // and body ($message_parts[1]).
         $header_lines = $message_parts[0];
-        $body = isset($message_parts[1])? $message_parts[1] : NULL;
+        $body = isset($message_parts[1]) ? $message_parts[1] : NULL;
+        // Process the headers - transform the string in an associative array
+        // where the keys are headers name and the values - headers value.
         foreach (explode("\r\n", $header_lines) as $header_line) {
           $header_parts = preg_split('/:\s+/', $header_line, 2);
           if (count($header_parts) == 2) {
--- src/RelaxedServiceProvider.php
+++ src/RelaxedServiceProvider.php
@@ -20,10 +20,11 @@ class RelaxedServiceProvider implements ServiceModifierInterface {
    */
   public function alter(ContainerBuilder $container) {
     if ($container->has('http_middleware.negotiation')) {
+      $negotiation = $container->getDefinition('http_middleware.negotiation');
       // Adds related as known format.
-      $container->getDefinition('http_middleware.negotiation')->addMethodCall('registerFormat', ['related', ['multipart/related']]);
+      $negotiation->addMethodCall('registerFormat', ['related', ['multipart/related']]);
       // Adds mixed as known format.
-      $container->getDefinition('http_middleware.negotiation')->addMethodCall('registerFormat', ['mixed', ['multipart/mixed']]);
+      $negotiation->addMethodCall('registerFormat', ['mixed', ['multipart/mixed']]);
     }
 
     // Override the access_check.rest.csrf class with a new class.
