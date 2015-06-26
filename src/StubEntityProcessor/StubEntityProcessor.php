95267707379aa0f78b7a3c93e4169ab45feb6876
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-06-26 10:01:59 +0300

    Refactor the StubEntityProcessor class and add an interface.

--- src/StubEntityProcessor/StubEntityProcessor.php
+++ src/StubEntityProcessor/StubEntityProcessor.php
@@ -10,18 +10,26 @@ namespace Drupal\relaxed\StubEntityProcessor;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 
-class StubEntityProcessor {
+class StubEntityProcessor implements StubEntityProcessorInterface {
 
   /**
-   * Processes an entity and saves stub entities, for entity reference fields,
-   * when the referenced entity does not exist. If the processed entity has an
-   * stub entity then update the entity.
-   *
-   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
-   *
-   * @return \Drupal\Core\Entity\ContentEntityInterface
+   * {@inheritdoc}
    */
   public function processEntity(ContentEntityInterface $entity) {
+    $this->saveStubEntities($entity);
+    $existing_entities = entity_load_multiple_by_properties($entity->getEntityTypeId(), array('uuid' => $entity->uuid()));
+    $existing_entity = reset($existing_entities);
+    // Update a stub entity with the correct values.
+    if ($existing_entity && !$entity->id()) {
+      $this->updateStubEntity($entity,$existing_entity);
+    }
+    return $entity;
+  }
+
+  /**
+   * Saves stub entities and update the target entity.
+   */
+  protected function saveStubEntities(ContentEntityInterface $entity) {
     foreach ($entity as $field_name => $field) {
       // For entity reference fields we should check if the referenced entity
       // exists or we should save a stub entity.
@@ -31,17 +39,17 @@ class StubEntityProcessor {
           // it doesn't exist.
           if (isset($item->entity_to_save)) {
             $entity_to_save = $item->entity_to_save;
-            $existent_entities = entity_load_multiple_by_properties(
+            $existing_entities = entity_load_multiple_by_properties(
               $item->entity_to_save->getEntityTypeId(),
               array('uuid' => $item->entity_to_save->uuid())
             );
-            $existent_entity = reset($existent_entities);
+            $existing_entity = reset($existing_entities);
             // Unset information about the entity_to_save.
             unset($entity->{$field_name}[$delta]->entity_to_save);
             // If the entity already exists, don't save the stub entity, just
             // complete the field with the correct target_id.
-            if ($existent_entity) {
-              $entity->{$field_name}[$delta] = array('target_id' => $existent_entity->id());
+            if ($existing_entity) {
+              $entity->{$field_name}[$delta] = array('target_id' => $existing_entity->id());
               continue;
             }
             // Save the stub entity and set the target_id value to the field item.
@@ -51,21 +59,20 @@ class StubEntityProcessor {
         }
       }
     }
+  }
 
-    $existent_entities = entity_load_multiple_by_properties($entity->getEntityTypeId(), array('uuid' => $entity->uuid()));
-    $existent_entity = reset($existent_entities);
-    // Update a stub entity with the correct values.
-    if ($existent_entity && !$entity->id()) {
-      $id_key = $entity->getEntityType()->getKey('id');
-      foreach ($existent_entity as $field_name => $field) {
-        if ($field_name != $id_key && $entity->{$field_name}->value) {
-          $existent_entity->{$field_name}->value = $entity->{$field_name}->value;
-        }
+  /**
+   * Updates a stub entity.
+   */
+  protected function updateStubEntity(ContentEntityInterface $entity, ContentEntityInterface $existing_entity) {
+    $id_key = $entity->getEntityType()->getKey('id');
+    foreach ($existing_entity as $field_name => $field) {
+      if ($field_name != $id_key && $entity->{$field_name}->value) {
+        $existing_entity->{$field_name}->value = $entity->{$field_name}->value;
       }
-      $entity = $existent_entity;
-      $entity->isDefaultRevision(TRUE);
     }
-    return $entity;
+    $entity = $existing_entity;
+    $entity->isDefaultRevision(TRUE);
   }
 
 }
