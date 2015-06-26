0dbc924668ca795eb6df27b760c97379d9dc7d34
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-06-26 11:11:54 +0300

    Inject the StubEntityProcessor in BulkDocs.

--- src/BulkDocs/BulkDocs.php
+++ src/BulkDocs/BulkDocs.php
@@ -5,6 +5,7 @@ namespace Drupal\relaxed\BulkDocs;
 use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\multiversion\Entity\WorkspaceInterface;
 use Drupal\multiversion\Workspace\WorkspaceManagerInterface;
+use Drupal\relaxed\StubEntityProcessor\StubEntityProcessorInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class BulkDocs implements BulkDocsInterface {
@@ -40,17 +41,20 @@ class BulkDocs implements BulkDocsInterface {
   static public function createInstance(ContainerInterface $container, WorkspaceManagerInterface $workspace_manager, WorkspaceInterface $workspace) {
     return new static(
       $workspace_manager,
-      $workspace
+      $workspace,
+      $container->get('relaxed.stub_entity_processor')
     );
   }
 
   /**
    * @param \Drupal\multiversion\Workspace\WorkspaceManagerInterface $workspace_manager
    * @param \Drupal\multiversion\Entity\WorkspaceInterface $workspace
+   * @param \Drupal\relaxed\StubEntityProcessor\StubEntityProcessorInterface $stub_entity_processor
    */
-  public function __construct(WorkspaceManagerInterface $workspace_manager, WorkspaceInterface $workspace) {
+  public function __construct(WorkspaceManagerInterface $workspace_manager, WorkspaceInterface $workspace, StubEntityProcessorInterface $stub_entity_processor) {
     $this->workspaceManager = $workspace_manager;
     $this->workspace = $workspace;
+    $this->stub_entity_processor = $stub_entity_processor;
   }
 
   /**
@@ -90,7 +94,7 @@ class BulkDocs implements BulkDocsInterface {
         // This will save stub entities in case the entity has entity reference
         // fields and a referenced entity does not exist or will update stub
         // entities with the correct values.
-        \Drupal::service('relaxed.stub_entity_processor')->processEntity($entity);
+        $this->stub_entity_processor->processEntity($entity);
 
         $entity->save();
 
