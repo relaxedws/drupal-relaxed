c6e025c0a7bf8a7910c5e4d078c79910427b5ca0
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-09-07 14:26:29 +0300

    Try to fix stub entities cretion and updates.

--- src/AllDocs/AllDocs.php
+++ src/AllDocs/AllDocs.php
@@ -183,6 +183,9 @@ class AllDocs implements AllDocsInterface {
         if ($this->includeDocs) {
           $entities = $this->entityManager->getStorage($entity_type_id)->loadMultiple($ids);
           foreach ($entities as $entity) {
+            if (strpos($entity->_rev->value, '1-101010101010101010101010') !== FALSE) {
+              continue;
+            }
             $rows[$entity->uuid()]['doc'] = $this->serializer->normalize($entity, 'json');
           }
         }
