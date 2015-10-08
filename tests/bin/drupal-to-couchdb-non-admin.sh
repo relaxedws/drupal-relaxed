1cd56fd97c2dc029f23170fa075dbc3093cece7b
timmillwood <tim@millwoodonline.co.uk>
2015-10-08 14:56:09 +0100

    updating bash scripts to work locally

--- tests/bin/drupal-to-couchdb-non-admin.sh
+++ tests/bin/drupal-to-couchdb-non-admin.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
 set -ev
+DRUPAL_ROOT=$1
+DRUPAL_DOMAIN=$2
 
 # Enable dependencies.
-mv $TRAVIS_BUILD_DIR/../drupal/core/modules/system/tests/modules/entity_test $TRAVIS_BUILD_DIR/../drupal/modules/entity_test
-mv $TRAVIS_BUILD_DIR/../drupal/modules/relaxed/tests/modules/relaxed_test $TRAVIS_BUILD_DIR/../drupal/modules/relaxed_test
+if [ ! -f $DRUPAL_ROOT/modules/entity_test ];
+then
+  ln -s $DRUPAL_ROOT/core/modules/system/tests/modules/entity_test $DRUPAL_ROOT/modules/entity_test
+fi
+
+if [ ! -f $DRUPAL_ROOT/modules/entity_test ];
+then
+  ln -s $DRUPAL_ROOT/modules/relaxed/tests/modules/relaxed_test $DRUPAL_ROOT/modules/relaxed_test
+fi
+
 drush en --yes entity_test, relaxed_test || true
 
 # Create a new role, add 'perform content replication' permission to this role
@@ -20,20 +30,23 @@ do
   curl -X POST \
        -H "Content-Type: application/json" \
        -d "$document" \
-       admin:admin@drupal.loc/relaxed/default;
+       $DRUPAL_DOMAIN/relaxed/default;
   sleep 2;
-done < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt
+done < $DRUPAL_ROOT/modules/relaxed/tests/fixtures/documents.txt
 
 # Create a target database and do the replication.
 curl -X PUT localhost:5984/target
 
+# Split the username:password from host in drupal_domain
+IFS='@' read -ra URL <<< "$DRUPAL_DOMAIN"
+
 # Run the replication.
-nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://replicator:replicator@drupal.loc/relaxed/default", "target": "http://localhost:5984/target", "worker_processes": 1}' http://localhost:5984/_replicate &
+nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://replicator:replicator@${URL[1]}/relaxed/default", "target": "http://localhost:5984/target", "worker_processes": 1}' http://localhost:5984/_replicate &
 sleep 120
 
 curl -X GET http://localhost:5984/target/_all_docs | tee /tmp/all_docs.txt
 
-COUNT=$(wc -l < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt)
+COUNT=$(wc -l < $DRUPAL_ROOT/modules/relaxed/tests/fixtures/documents.txt)
 USERS=3
 COUNT=$(($COUNT + $USERS));
 test 1 -eq $(egrep -c "(\"total_rows\"\:$COUNT)" /tmp/all_docs.txt)
