d498b4a8fa80e360f18686351951f1c305fbe836
dickolsson <dick.olsson@senzilla.com>
2015-11-10 15:04:40 +0000

    Lock PouchDB to major version 4 to stay compatible with out API

--- tests/bin/pouchdb.sh
+++ tests/bin/pouchdb.sh
@@ -7,7 +7,7 @@ npm install -g mocha-phantomjs
 npm install chai
 npm install es5-shim
 npm install mocha
-npm install pouchdb
+npm install pouchdb@"~4.0"
 
 mv $TRAVIS_BUILD_DIR/../drupal/core/modules/system/tests/modules/entity_test $TRAVIS_BUILD_DIR/../drupal/modules/entity_test
 mv $TRAVIS_BUILD_DIR/../drupal/modules/relaxed/tests/pouchdb/test.html $TRAVIS_BUILD_DIR/../drupal/test.html
