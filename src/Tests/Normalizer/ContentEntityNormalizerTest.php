24e2abab039211d71d2bdb314e083dd6f651acad
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-11-11 15:50:54 +0200

    Normalizer tests refactoring, remove deprecated functions.

--- src/Tests/Normalizer/ContentEntityNormalizerTest.php
+++ src/Tests/Normalizer/ContentEntityNormalizerTest.php
@@ -17,7 +17,19 @@ use Drupal\entity_test\Entity\EntityTestMulRev;
  */
 class ContentEntityNormalizerTest extends NormalizerTestBase {
 
-  public static $modules = array('serialization', 'system', 'field', 'entity_test', 'text', 'filter', 'user', 'key_value', 'multiversion', 'rest', 'relaxed');
+  public static $modules = [
+    'serialization',
+    'system',
+    'field',
+    'entity_test',
+    'text',
+    'filter',
+    'user',
+    'key_value',
+    'multiversion',
+    'rest',
+    'relaxed'
+  ];
 
   protected $entityClass = 'Drupal\entity_test\Entity\EntityTest';
 
@@ -27,20 +39,21 @@ class ContentEntityNormalizerTest extends NormalizerTestBase {
     // @todo: {@link https://www.drupal.org/node/2600468 Attach a file field.}
 
     // Create a test entity to serialize.
-    $this->values = array(
+    $this->values = [
       'name' => $this->randomMachineName(),
       'user_id' => 0,
-      'field_test_text' => array(
+      'field_test_text' => [
         'value' => $this->randomMachineName(),
         'format' => 'full_html',
-      ),
-    );
+      ],
+    ];
 
     $this->entity = EntityTestMulRev::create($this->values);
     $this->entity->save();
   }
 
-  public function testNormalize() {
+  public function testNormalizer() {
+    // Test normalize.
     $expected = array(
       '@context' => array(
         '_id' => '@id',
@@ -81,43 +94,41 @@ class ContentEntityNormalizerTest extends NormalizerTestBase {
     $normalized = $this->serializer->normalize($this->entity);
 
     foreach (array_keys($expected) as $key) {
-      $this->assertEqual($expected[$key], $normalized[$key], "Field $key is normalized correctly.");
+      $this->assertEquals($expected[$key], $normalized[$key], "Field $key is normalized correctly.");
     }
-    $this->assertEqual(array_diff_key($normalized, $expected), array(), 'No unexpected data is added to the normalized array.');
+    $this->assertEquals(array_diff_key($normalized, $expected), [], 'No unexpected data is added to the normalized array.');
 
     // Test normalization when is set the revs query parameter.
     $parts = explode('-', $this->entity->_rev->value);
-    $expected['_revisions'] = array(
-      'ids' => array($parts[1]),
+    $expected['_revisions'] = [
+      'ids' => [$parts[1]],
       'start' => (int) $parts[0],
-    );
+    ];
 
-    $normalized = $this->serializer->normalize($this->entity, NULL, array('query' => array('revs' => TRUE)));
+    $normalized = $this->serializer->normalize($this->entity, NULL, ['query' => ['revs' => TRUE]]);
 
     foreach (array_keys($expected) as $key) {
-      $this->assertEqual($expected[$key], $normalized[$key], "Field $key is normalized correctly.");
+      $this->assertEquals($expected[$key], $normalized[$key], "Field $key is normalized correctly.");
     }
     $this->assertTrue($expected['_revisions']['start'] === $normalized['_revisions']['start'], "Correct data type for the start field.");
-    $this->assertEqual(array_diff_key($normalized, $expected), array(), 'No unexpected data is added to the normalized array.');
+    $this->assertEquals(array_diff_key($normalized, $expected), [], 'No unexpected data is added to the normalized array.');
 
     // @todo {@link https://www.drupal.org/node/2600460 Test context switches.}
-  }
 
-  public function testSerialize() {
+    // Test serialize.
     $normalized = $this->serializer->normalize($this->entity);
     $expected = json_encode($normalized);
     // Paranoid test because JSON serialization is tested elsewhere.
     $actual = $this->serializer->serialize($this->entity, 'json');
-    $this->assertIdentical($actual, $expected, 'Entity serializes correctly to JSON.');
-  }
+    $this->assertSame($actual, $expected, 'Entity serializes correctly to JSON.');
 
-  public function testDenormalize() {
+    // Test denormalize.
     $normalized = $this->serializer->normalize($this->entity);
     $denormalized = $this->serializer->denormalize($normalized, $this->entityClass, 'json');
-    $this->assertTrue($denormalized instanceof $this->entityClass, SafeMarkup::format('Denormalized entity is an instance of @class', array('@class' => $this->entityClass)));
-    $this->assertIdentical($denormalized->getEntityTypeId(), $this->entity->getEntityTypeId(), 'Expected entity type found.');
-    $this->assertIdentical($denormalized->bundle(), $this->entity->bundle(), 'Expected entity bundle found.');
-    $this->assertIdentical($denormalized->uuid(), $this->entity->uuid(), 'Expected entity UUID found.');
+    $this->assertTrue($denormalized instanceof $this->entityClass, SafeMarkup::format('Denormalized entity is an instance of @class', ['@class' => $this->entityClass]));
+    $this->assertSame($denormalized->getEntityTypeId(), $this->entity->getEntityTypeId(), 'Expected entity type found.');
+    $this->assertSame($denormalized->bundle(), $this->entity->bundle(), 'Expected entity bundle found.');
+    $this->assertSame($denormalized->uuid(), $this->entity->uuid(), 'Expected entity UUID found.');
 
     // @todo {@link https://www.drupal.org/node/2600460 Test context switches.}
   }
