931a899a464929cf64619e915e47a07cbc35183f
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-11-12 13:58:05 +0200

    Test couchdb-to-drupal and drupal-to-couchdb replications just with anonymous user.

--- tests/bin/couchdb-to-drupal.sh
+++ tests/bin/couchdb-to-drupal.sh
@@ -7,6 +7,13 @@ mv $TRAVIS_BUILD_DIR/../drupal/core/modules/system/tests/modules/entity_test $TR
 mv $TRAVIS_BUILD_DIR/../drupal/modules/relaxed/tests/modules/relaxed_test $TRAVIS_BUILD_DIR/../drupal/modules/relaxed_test
 drush en --yes entity_test, relaxed_test || true
 
+# Create a new role, add 'perform content replication' permission to this role
+# and create a user with this role.
+drush role-create 'Replicator'
+drush role-add-perm 'Replicator' 'perform content replication'
+drush user-create replicator --mail="replicator@example.com" --password="replicator"
+drush user-add-role 'Replicator' replicator
+
 # Create a target database and do the replication.
 curl -X PUT localhost:5984/source
 
@@ -17,7 +24,6 @@ do
        -H "Content-Type: application/json" \
        -d "$document" \
        localhost:5984/source;
-  sleep 2;
 done < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt
 
 # Get all docs from couchdb db.
@@ -26,8 +32,8 @@ curl -X GET http://localhost:5984/source/_all_docs
 drush cr
 
 # Run the replication.
-nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://localhost:5984/source", "target": "http://admin:admin@drupal.loc/relaxed/default", "worker_processes": 1}' http://localhost:5984/_replicate &
-sleep 120
+nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://localhost:5984/source", "target": "http://replicator:replicator@drupal.loc/relaxed/default"}' http://localhost:5984/_replicate &
+sleep 60
 
 curl -X GET http://admin:admin@drupal.loc/relaxed/default/_all_docs | tee /tmp/all_docs.txt
 
@@ -38,6 +44,6 @@ sudo cat /var/log/apache2/error.log
 #-----------------------------------
 
 COUNT=$(wc -l < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt)
-USERS=2
+USERS=3
 COUNT=$(($COUNT + $USERS));
 test 1 -eq $(egrep -c "(\"total_rows\"\:$COUNT)" /tmp/all_docs.txt)
