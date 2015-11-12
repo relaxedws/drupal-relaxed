21deb7fd295840d4a3fa394afc9148faf0c81d22
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-11-12 12:15:54 +0200

    Remove deprecated from AttachmentResourceTest.

--- src/Tests/AttachmentResourceTest.php
+++ src/Tests/AttachmentResourceTest.php
@@ -8,6 +8,10 @@
 namespace Drupal\relaxed\Tests;
 
 use Drupal\Component\Serialization\Json;
+use Drupal\entity_test\Entity\EntityTestRev;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\file\Entity\File;
 
 /**
  * Tests the /db/doc/attachment.
@@ -16,7 +20,12 @@ use Drupal\Component\Serialization\Json;
  */
 class AttachmentResourceTest extends ResourceTestBase {
 
-  public static $modules = array('rest', 'entity_test', 'file', 'image');
+  public static $modules = [
+    'rest',
+    'entity_test',
+    'file',
+    'image'
+  ];
 
   /**
    * @var \Drupal\file\FileInterface[]
@@ -32,74 +41,68 @@ class AttachmentResourceTest extends ResourceTestBase {
     parent::setUp();
 
     // Create a File field for testing.
-    entity_create('field_storage_config', array(
+    FieldStorageConfig::create([
         'field_name' => 'field_test_file',
         'entity_type' => 'entity_test_rev',
         'type' => 'file',
         'cardinality' => 4,
         'translatable' => FALSE,
-      ))->save();
-    entity_create('field_config', array(
+      ])->save();
+    FieldConfig::create([
         'entity_type' => 'entity_test_rev',
         'field_name' => 'field_test_file',
         'bundle' => 'entity_test_rev',
         'label' => 'Test file-field',
-        'widget' => array(
+        'widget' => [
           'type' => 'file',
           'weight' => 0,
-        ),
-      ))->save();
+        ],
+      ])->save();
     file_put_contents('public://example1.txt', $this->randomMachineName());
-    $this->files['1'] = entity_create('file', array(
-      'uri' => 'public://example1.txt',
-    ));
+    $this->files['1'] = File::create(['uri' => 'public://example1.txt']);
     $this->files['1']->save();
     file_put_contents('public://example2.txt', $this->randomMachineName());
-    $this->files['2'] = entity_create('file', array(
-     'uri' => 'public://example2.txt',
-    ));
+    $this->files['2'] = File::create(['uri' => 'public://example2.txt']);
     $this->files['2']->save();
 
     // Create a Image field for testing.
-    entity_create('field_storage_config', array(
+    FieldStorageConfig::create([
       'field_name' => 'field_test_image',
       'entity_type' => 'entity_test_rev',
       'type' => 'image',
       'cardinality' => 3,
       'translatable' => FALSE,
-    ))->save();
-    entity_create('field_config', array(
+    ])->save();
+    FieldConfig::create([
       'entity_type' => 'entity_test_rev',
       'field_name' => 'field_test_image',
       'bundle' => 'entity_test_rev',
       'label' => 'Test image-field',
-      'widget' => array(
+      'widget' => [
         'type' => 'image',
         'weight' => 0,
-      ),
-    ))->save();
+      ],
+    ])->save();
     file_unmanaged_copy(DRUPAL_ROOT . '/core/misc/druplicon.png', 'public://example.png');
-    $this->files['3'] = entity_create('file', array(
-      'uri' => 'public://example.png',
-    ));
+    $this->files['3'] = File::create(['uri' => 'public://example.png']);
     $this->files['3']->save();
 
-    $values = array(
+    $values = [
       'name' => $this->randomMachineName(),
       'user_id' => 0,
-      'field_test_file' => array(
-        array(
+      'field_test_file' => [
+        [
           'target_id' => $this->files['1']->id(),
           'display' => 1,
           'description' => $this->randomMachineName(),
-        ),
-        array(
+        ],
+        [
           'target_id' => $this->files['2']->id(),
           'display' => 1,
           'description' => $this->randomMachineName(),
-        ),
-      ),
-      'field_test_image' => array(
+        ],
+      ],
+      'field_test_image' => [
         'target_id' => $this->files['3']->id(),
         'display' => 1,
         'description' => $this->randomMachineName(),
@@ -107,9 +110,9 @@ class AttachmentResourceTest extends ResourceTestBase {
         'title' => $this->randomMachineName(),
         'width' => 200,
         'height' => 100,
-      ),
-    );
-    $this->entity = entity_create('entity_test_rev', $values);
+      ],
+    ];
+    $this->entity = EntityTestRev::create($values);
     $this->entity->save();
   }
 
@@ -218,9 +221,7 @@ class AttachmentResourceTest extends ResourceTestBase {
 
     $file_uri = 'public://new_example.txt';
     file_put_contents($file_uri, $this->randomMachineName());
-    $file_stub = entity_create('file', array(
-      'uri' => $file_uri,
-    ));
+    $file_stub = File::create(['uri' => $file_uri]);
     $serialized = $serializer->serialize($file_stub, 'stream');
 
     $field_name = 'field_test_file';
@@ -236,7 +237,7 @@ class AttachmentResourceTest extends ResourceTestBase {
     $this->assertTrue(!empty($file), 'File was saved.');
     $this->assertEqual($file->getFileUri(), $file_uri, 'File was saved with the correct URI.');
 
-    $entity = entity_load('entity_test_rev', $this->entity->id());
+    $entity = EntityTestRev::load($this->entity->id());
     $this->assertEqual($entity->{$field_name}->get(0)->target_id, $file->id(), 'File was attached to the entity.');
   }
 
@@ -257,9 +258,9 @@ class AttachmentResourceTest extends ResourceTestBase {
     $data = Json::decode($response);
     $this->assertTrue(!empty($data['ok']), 'DELETE request returned ok.');
 
-    $file = entity_load('file',  $this->files['2']->id());
+    $file = File::load($this->files['2']->id());
     $this->assertTrue(empty($file), 'The file was deleted.');
-    $entity = entity_load('entity_test_rev', $this->entity->id());
+    $entity = EntityTestRev::load($this->entity->id());
     $this->assertEqual($entity->{$field_name}->count(), 1, 'The file does not exist on the entity any more.');
   }
 
