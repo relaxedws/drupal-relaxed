0447c5f3c045c3feaea5defc68ac2d49166cbbc6
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-11-17 14:29:58 +0200

    fixup! Load fixtures from the documents.txt as we do for all replication types.

--- tests/bin/pouchdb.sh
+++ tests/bin/pouchdb.sh
@@ -3,12 +3,7 @@
 set -ev
 
 # Install mocha-phantomjs, pouchdb and other dependences.
-npm install -g mocha-phantomjs
-npm install chai
-npm install es5-shim
-npm install mocha
-npm install pouchdb
-npm install request
+npm install ../pouchdb
 
 mv ~/www/core/modules/system/tests/modules/entity_test ~/www/modules/entity_test
 mv ~/www/modules/relaxed/tests/pouchdb/test.html ~/www/test.html
