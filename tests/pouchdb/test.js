eab7ee5758a2f74d8817c5d40e21407b4c797370
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-11-18 12:08:02 +0200

    fixup! Load fixtures from the documents.txt as we do for all replication types.

--- tests/pouchdb/test.js
+++ tests/pouchdb/test.js
@@ -1,5 +1,10 @@
 var request = require('request');
-var docsInfo = request('http://localhost:8080/modules/relaxed/tests/fixtures/documents.txt');
+var docsInfo = '';
+request('http://localhost:8080/modules/relaxed/tests/fixtures/documents.txt', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    docsInfo = body;
+  }
+});
 var docs = docsInfo.split(/\r\n|\n/);
 
 // Enable debugging mode.
