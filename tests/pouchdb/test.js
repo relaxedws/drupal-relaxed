1de757045e70ec79be2b97d92e7d636823dabba3
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-11-18 17:43:52 +0200

    fixup! Load documents.txt without request library.

--- tests/pouchdb/test.js
+++ tests/pouchdb/test.js
@@ -1,27 +1,26 @@
-var getJSON = function(url) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url, true);
-    xhr.responseType = 'txt';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-    xhr.send();
-  });
+var getJSON = function(url, successHandler, errorHandler) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', url, true);
+  xhr.responseType = 'txt';
+  xhr.onload = function() {
+    var status = xhr.status;
+    if (status == 200) {
+      successHandler && successHandler(xhr.response);
+    } else {
+      errorHandler && errorHandler(status);
+    }
+  };
+  xhr.send();
 };
 
 var docs = [];
-var document = 'http://localhost:8080/modules/relaxed/tests/fixtures/documents.txt';
-getJSON(document).then(function(data) {
+getJSON('http://drupal8dev.loc/documents.txt', function(data) {
   var lines = data.split(/\r\n|\n/);
   // Create an array with all docs.
   for(var line = 0; line < lines.length; line++) {
-    docs.push(JSON.parse(lines[line]));
+    if (lines[line]) {
+      docs.push(JSON.parse(lines[line]));
+    }
   }
 }, function(status) {
   console.log('Something went wrong.');
