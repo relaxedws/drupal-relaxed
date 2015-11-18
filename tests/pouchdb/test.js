156bd2f7da6ecfad93191835328ed1edf9f8fe86
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-11-18 18:23:24 +0200

    fixup! Load documents.txt without request library.

--- tests/pouchdb/test.js
+++ tests/pouchdb/test.js
@@ -13,19 +13,6 @@ var getJSON = function(url, successHandler, errorHandler) {
   xhr.send();
 };
 
-var docs = [];
-getJSON('http://drupal8dev.loc/documents.txt', function(data) {
-  var lines = data.split(/\r\n|\n/);
-  // Create an array with all docs.
-  for(var line = 0; line < lines.length; line++) {
-    if (lines[line]) {
-      docs.push(JSON.parse(lines[line]));
-    }
-  }
-}, function(status) {
-  console.log('Something went wrong.');
-});
-
 // Enable debugging mode.
 PouchDB.debug.enable('*');
 //PouchDB.debug.enable('pouchdb:api');
@@ -36,38 +23,49 @@ describe('Test replication', function () {
   it('Test basic push replication', function (done) {
     var db = new PouchDB('pouch_to_drupal');
     var remote = new PouchDB('http://replicator:replicator@localhost:8080/relaxed/default');
-    db.bulkDocs({ docs: docs }, {}, function (err, results) {
-      db.replicate.to(remote, function (err, result) {
-        result.ok.should.equal(true);
-        result.docs_written.should.equal(docs.length);
-        db.info(function (err, info) {
-          console.log(err);
-          verifyInfo(info, {
-            update_seq: 12,
-            doc_count: 12
+    getJSON('http://drupal8dev.loc/documents.txt', function(data) {
+      var lines = data.split(/\r\n|\n/);
+      // Create an array with all docs.
+      for(var line = 0; line < lines.length; line++) {
+        if (lines[line]) {
+          docs.push(JSON.parse(lines[line]));
+        }
+      }
+
+      db.bulkDocs({ docs: docs }, {}, function (err, results) {
+        db.replicate.to(remote, function (err, result) {
+          result.ok.should.equal(true);
+          result.docs_written.should.equal(docs.length);
+          db.info(function (err, info) {
+            console.log(err);
+            verifyInfo(info, {
+              update_seq: 12,
+              doc_count: 12
+            });
+            done();
           });
-          done();
         });
       });
+
+    }, function(status) {
+      console.log('Something went wrong.');
     });
   });
 
   it('Test basic pull replication', function (done) {
     var db = new PouchDB('drupal_to_pouch');
     var remote = new PouchDB('http://replicator:replicator@localhost:8080/relaxed/default');
-    //remote.bulkDocs({ docs: docs }, {}, function (err, results) {
-      db.replicate.from(remote, {}, function (err, result) {
-        result.ok.should.equal(true);
-        result.docs_written.should.equal(14);
-        db.info(function (err, info) {
-          verifyInfo(info, {
-            update_seq: 14,
-            doc_count: 14
-          });
-          done();
+    db.replicate.from(remote, {}, function (err, result) {
+      result.ok.should.equal(true);
+      result.docs_written.should.equal(14);
+      db.info(function (err, info) {
+        verifyInfo(info, {
+          update_seq: 14,
+          doc_count: 14
         });
+        done();
       });
-    //});
+    });
   });
 });
 
