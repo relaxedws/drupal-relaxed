4c47ca9db15d58df7223cf314fdb67a89fb11d36
dickolsson <dick.olsson@senzilla.com>
2015-11-28 17:00:38 +0000

    debug

--- tests/bin/couchdb-to-drupal.sh
+++ tests/bin/couchdb-to-drupal.sh
@@ -5,7 +5,7 @@ set -ev
 # Enable dependencies.
 mv ~/www/core/modules/system/tests/modules/entity_test ~/www/modules/entity_test
 mv ~/www/modules/relaxed/tests/modules/relaxed_test ~/www/modules/relaxed_test
-php ~/drush.phar en --yes entity_test, relaxed_test || true
+php ~/drush.phar en --yes relaxed_test, dblog || true
 
 # Create a new role, add 'perform content replication' permission to this role
 # and create a user with this role.
@@ -29,12 +29,10 @@ done < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt
 # Get all docs from couchdb db.
 curl -X GET http://localhost:5984/source/_all_docs
 
-php ~/drush.phar cache-rebuild
+php ~/drush.phar watchdog-show --count=100
 
 # Run the replication.
-nohup curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://localhost:5984/source", "target": "http://replicator:replicator@localhost:8080/relaxed/default"}' http://localhost:5984/_replicate &
-sleep 60
-
+curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"source": "http://localhost:5984/source", "target": "http://replicator:replicator@localhost:8080/relaxed/default", "worker_processes": 1}' http://localhost:5984/_replicate
 curl -X GET http://admin:admin@localhost:8080/relaxed/default/_all_docs | tee /tmp/all_docs.txt
 
 COUNT=$(wc -l < $TRAVIS_BUILD_DIR/tests/fixtures/documents.txt)
