948be0ae65ffd9f8f866de251c957d6f079cbf82
dickolsson <dick.olsson@senzilla.com>
2015-11-30 12:06:30 +0000

    Simplify the PouchDB tests by no longer hard coding the number of expected documents.

--- tests/pouchdb/test.js
+++ tests/pouchdb/test.js
@@ -1,79 +1,55 @@
+var baseUrl = 'http://replicator:replicator@localhost:8080';
+//PouchDB.debug.enable('*');
+
 var getFixtures = function(url, successHandler, errorHandler) {
   var xhr = new XMLHttpRequest();
   xhr.open('get', url, true);
   xhr.responseType = 'text';
   xhr.onload = function() {
-    var status = xhr.status;
-    if (status == 200) {
-      successHandler && successHandler(xhr.response);
+    if (xhr.status == 200) {
+      // Parse the response into an array of JSON fixtures.
+      var fixtures = [];
+      var lines = xhr.response.split(/\r\n|\n/);
+      for(var i = 0; i < lines.length; i++) {
+        if (lines[i]) {
+          fixtures.push(JSON.parse(lines[i]));
+        }
+      }
+      successHandler && successHandler(fixtures);
     } else {
-      errorHandler && errorHandler(status);
+      errorHandler && errorHandler(xhr.status);
     }
   };
   xhr.send();
 };
 
-// Enable debugging mode.
-PouchDB.debug.enable('*');
-//PouchDB.debug.enable('pouchdb:api');
-//PouchDB.debug.enable('pouchdb:http');
-
-var baseUrl = 'http://replicator:replicator@localhost:8080';
 describe('Test replication', function () {
-
   it('Test basic push replication', function (done) {
-    var docs = [];
     var db = new PouchDB('pouch_to_drupal');
     var remote = new PouchDB(baseUrl + '/relaxed/default');
-    getFixtures(baseUrl + '/documents.txt', function(data) {
-      var lines = data.split(/\r\n|\n/);
-      // Create an array with all docs.
-      for(var line = 0; line < lines.length; line++) {
-        if (lines[line]) {
-          docs.push(JSON.parse(lines[line]));
-        }
-      }
-
+    getFixtures(baseUrl + '/documents.txt', function(docs) {
       db.bulkDocs({ docs: docs }, {}, function (err, results) {
         db.replicate.to(remote, function (err, result) {
           result.ok.should.equal(true);
+          result.doc_write_failures.should.equal(0);
           result.docs_written.should.equal(docs.length);
-          db.info(function (err, info) {
-            console.log(err);
-            verifyInfo(info, {
-              update_seq: 12,
-              doc_count: 12
-            });
-            done();
-          });
+          done();
         });
       });
-
-    }, function(status) {
-      console.log('Something went wrong.');
     });
   });
 
   it('Test basic pull replication', function (done) {
     var db = new PouchDB('drupal_to_pouch');
     var remote = new PouchDB(baseUrl + '/relaxed/default');
-    db.replicate.from(remote, {}, function (err, result) {
-      result.ok.should.equal(true);
-      result.docs_written.should.equal(15);
-      db.info(function (err, info) {
-        verifyInfo(info, {
-          update_seq: 15,
-          doc_count: 15
-        });
+    getFixtures(baseUrl + '/documents.txt', function(docs) {
+      db.replicate.from(remote, {}, function (err, result) {
+        result.ok.should.equal(true);
+        result.doc_write_failures.should.equal(0);
+        // Add the 3 users that already exist in the Drupal site.
+        result.docs_written.should.equal(docs.length + 3);
         done();
       });
     });
   });
 });
-
-function verifyInfo(info, expected) {
-  if (!testUtils.isCouchMaster()) {
-    info.update_seq.should.equal(expected.update_seq, 'update_seq');
-  }
-  info.doc_count.should.equal(expected.doc_count, 'doc_count');
-}
