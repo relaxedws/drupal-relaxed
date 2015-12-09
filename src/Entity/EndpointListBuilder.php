3a7482a19576782ddfbb4f2862e57be7385e8562
dickolsson <dick.olsson@senzilla.com>
2015-12-09 20:21:44 +0000

    Added CSS and icons to endpoints with errors.

--- src/Entity/EndpointListBuilder.php
+++ src/Entity/EndpointListBuilder.php
@@ -9,18 +9,29 @@ namespace Drupal\relaxed\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Routing\UrlGeneratorTrait;
 
 /**
  * Provides a listing of Endpoint entities.
  */
 class EndpointListBuilder extends ConfigEntityListBuilder {
 
+  use UrlGeneratorTrait;
+
+  /**
+   * @var bool
+   */
+  protected $hasError = FALSE;
+
   /**
    * {@inheritdoc}
    */
   public function buildHeader() {
     $header['label'] = $this->t('Endpoint');
-    $header['id'] = $this->t('Machine name');
+    $header['id'] = [
+      'class' => [RESPONSIVE_PRIORITY_LOW],
+      'data' => $this->t('Machine name'),
+    ];
     return $header + parent::buildHeader();
   }
 
@@ -29,20 +40,38 @@ class EndpointListBuilder extends ConfigEntityListBuilder {
    */
   public function buildRow(EntityInterface $entity) {
     $row['data'] = [
-      'label' => $entity->label(),
+      'label' => [
+        'class' => [
+          'relaxed-endpoint-check__status-title',
+          'relaxed-endpoint-check__status-icon',
+        ],
+        'data' => $entity->label(),
+      ],
       'id' => $entity->id(),
       'operations' => [
         'data' => $this->buildOperations($entity),
       ],
     ];
-    $row['class'] = 'color-error';
+    $row['class'] = ['relaxed-endpoint-check__entry'];
     $checks = \Drupal::service('plugin.manager.endpoint_check')->run($entity);
+    $row_has_error = FALSE;
     foreach ($checks as $check) {
-      if ($check['result'] === true) {
-        $row['class'] = 'color-success';
+      switch ($check['result']) {
+        case FALSE:
+          $row_has_error = TRUE;
+          break;
       }
     }
 
+    if ($row_has_error) {
+      $this->hasError = TRUE;
+      $row['data']['label']['class'][] = 'relaxed-endpoint-check__status-icon--error';
+      $row['class'][] = 'color-error';
+    }
+    else {
+      $row['data']['label']['class'][] = 'relaxed-endpoint-check__status-icon--ok';
+      $row['class'][] = 'color-ok';
+    }
     return $row;
   }
 
@@ -51,7 +80,20 @@ class EndpointListBuilder extends ConfigEntityListBuilder {
    */
   public function render() {
     $build['endpoints'] = parent::render();
+    $build['endpoints']['table']['#attached'] = [
+      'library' => [
+        'relaxed/drupal.relaxed.admin',
+      ],
+    ];
     $build['form'] = \Drupal::formBuilder()->getForm('Drupal\relaxed\Form\EndpointSetupForm');
+
+    // Indicate to the user that there is a problem with one of the endpoints.
+    if ($this->hasError) {
+      drupal_set_message(
+        $this->t('One or more problems were detected with your endpoints. Check the the <a href=":status">status report</a> for more information.', [':status' => $this->url('system.status')]),
+        'error'
+      );
+    }
     return $build;
   }
 
