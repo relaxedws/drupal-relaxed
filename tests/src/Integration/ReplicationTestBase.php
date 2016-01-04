92b7f600511b169a8099ab709219d8dd3662a2b6
Andrei Jechiu <andrei.jechiu@gmail.com>
2016-01-04 16:38:15 +0200

    Revert "Move replication tests in one method to improve the test running speed."
    This reverts commit 85491108ad1b5574862ee962231373c3aa96f00e.

--- tests/src/Integration/ReplicationTestBase.php
+++ tests/src/Integration/ReplicationTestBase.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains \Drupal\Tests\relaxed\Integration\ReplicationTest.
+ * Contains \Drupal\Tests\relaxed\Integration\ReplicationTestBase.
  */
 
 namespace Drupal\Tests\relaxed\Integration;
@@ -14,10 +14,7 @@ use Exception;
 use Relaxed\Replicator\ReplicationTask;
 use Relaxed\Replicator\Replicator;
 
-/**
- * @group relaxed
- */
-class ReplicationTest extends KernelTestBase {
+abstract class ReplicationTestBase extends KernelTestBase {
 
   protected $strictConfigSchema = FALSE;
 
@@ -102,78 +99,6 @@ class ReplicationTest extends KernelTestBase {
     } else {
       $this->fail("Error when reading documents.txt");
     }
-  }
-
-  /**
-   * Test the replication using CouchDB and PHP replicators.
-   */
-  public function testReplication() {
-
-    // Test the replication using the CouchDB replicator.
-
-    // Run CouchDB to Drupal replication.
-    $this->couchDbReplicate($this->couchdb_url . "/$this->source_db", 'http://replicator:replicator@localhost:8080/relaxed/default');
-    sleep(10);
-
-    // Run Drupal to Drupal replication.
-    $this->couchDbReplicate('http://replicator:replicator@localhost:8080/relaxed/default', 'http://replicator:replicator@localhost:8081/relaxed/default');
-    sleep(10);
-
-    // Run Drupal to CouchDB replication.
-    $this->couchDbReplicate('http://replicator:replicator@localhost:8081/relaxed/default', $this->couchdb_url . "/$this->target_db");
-    sleep(10);
-
-    // Get all docs from CouchDB target db.
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-      CURLOPT_HTTPGET => TRUE,
-      CURLOPT_RETURNTRANSFER => TRUE,
-      CURLOPT_URL => "$this->couchdb_url/$this->target_db/_all_docs",
-    ]);
-    $response = curl_exec($curl);
-    $this->assertContains('"total_rows":14', $response, 'The request returned the correct number of docs.');
-    curl_close($curl);
-
-    // Delete target database.
-    $response_code = $this->deleteDb($this->target_db);
-    $this->assertEquals(200, $response_code);
-
-    // Test the replication using the PHP replicator.
-
-    // Create a target database.
-    $response_code = $this->createDb($this->target_db);
-    $this->assertEquals(201, $response_code);
-
-    // Run CouchDB to Drupal replication.
-    $this->phpReplicate('{"source": {"dbname": "' . $this->source_db . '"}, "target": {"host": "localhost", "path": "relaxed", "port": 8080, "user": "replicator", "password": "replicator", "dbname": "default", "timeout": 10}}');
-    sleep(10);
-
-    // Run Drupal to Drupal replication.
-    $this->phpReplicate('{"source": {"host": "localhost", "path": "relaxed", "port": 8080, "user": "replicator", "password": "replicator", "dbname": "default", "timeout": 10}, "target": {"host": "localhost", "path": "relaxed", "port": 8081, "user": "replicator", "password": "replicator", "dbname": "default", "timeout": 10}}');
-    sleep(10);
-
-    // Run Drupal to CouchDB replication.
-    $this->phpReplicate('{"source": {"host": "localhost", "path": "relaxed", "port": 8081, "user": "replicator", "password": "replicator", "dbname": "default", "timeout": 10}, "target": {"dbname": "' . $this->target_db . '"}}');
-    sleep(10);
-
-    // Get all docs from CouchDB target db.
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-      CURLOPT_HTTPGET => TRUE,
-      CURLOPT_RETURNTRANSFER => TRUE,
-      CURLOPT_URL => "$this->couchdb_url/$this->target_db/_all_docs",
-    ]);
-    $response = curl_exec($curl);
-    $this->assertContains('"total_rows":14', $response, 'The request returned the correct number of docs.');
-    curl_close($curl);
-
-    // Delete source database.
-    $response_code = $this->deleteDb($this->source_db);
-    $this->assertEquals(200, $response_code);
-
-    // Delete target database.
-    $response_code = $this->deleteDb($this->target_db);
-    $this->assertEquals(200, $response_code);
   }
 
   /**
