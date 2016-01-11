ef636d3750f0675a9037de3b321adca4758b2f3c
johnstorey <johnstorey@578960.no-reply.drupal.org>
2016-01-11 09:01:58 +0000

    Issue #2578573 by johnstorey: ParamConverter\DbConverter missing docblocks

--- src/ParamConverter/DbConverter.php
+++ src/ParamConverter/DbConverter.php
@@ -23,6 +23,15 @@ class DbConverter implements ParamConverterInterface {
   /**
    * Converts a UUID into an existing entity.
    *
+   * @param int $entity_id
+   *   The raw value.
+   * @param mixed $definition
+   *   The parameter definition provided in the route options.
+   * @param string $name
+   *   The name of the parameter.
+   * @param array $defaults
+   *   The route defaults array.
+   *
    * @return string | \Drupal\Core\Entity\EntityInterface
    *   The entity if it exists in the database or else the original UUID string.
    * @todo {@link https://www.drupal.org/node/2600370 Fall back to a stub entity instead of UUID string.}
