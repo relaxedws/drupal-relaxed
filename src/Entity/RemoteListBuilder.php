6ab2e795d73bdb2bedb9586f387ebed6fafd4c40
Tim Millwood <tim@millwoodonline.co.uk>
2016-04-15 15:50:31 +0100

    adding the cheeky little drupal_set_message() back

--- src/Entity/RemoteListBuilder.php
+++ src/Entity/RemoteListBuilder.php
@@ -72,4 +72,23 @@ class RemoteListBuilder extends ConfigEntityListBuilder {
     return $row;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    $build['remotes'] = parent::render();
+    $build['remotes']['table']['#attached'] = [
+      'library' => [
+        'relaxed/drupal.relaxed.admin',
+      ],
+    ];
+    // Indicate to the user that there is a problem with one of the endpoints.
+    if ($this->hasError) {
+      drupal_set_message(
+        $this->t('One or more problems were detected with your endpoints. Check the the <a href=":status">status report</a> for more information.', [':status' => $this->url('system.status')]),
+        'error'
+      );
+    }
+    return $build;
+  }
 }
