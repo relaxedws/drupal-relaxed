41e89ea3b18003d4bbb64a706632ea4f829731c4
Andrei Jechiu <andrei.jechiu@gmail.com>
2016-06-21 17:42:52 +0200

    Rest method normalize fixes.

--- src/Entity/RestResourceConfig.php
+++ src/Entity/RestResourceConfig.php
@@ -5,15 +5,21 @@ namespace Drupal\relaxed\Entity;
 use Drupal\rest\Entity\RestResourceConfig as CoreRestResourceConfig;
 
 class RestResourceConfig extends CoreRestResourceConfig {
-  
+
   /**
    * {@inheritdoc}
    */
   protected function normalizeRestMethod($method) {
-    $valid_methods = ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'HEAD'];
-    $normalised_method = strtoupper($method);
-    if (!in_array($normalised_method, $valid_methods)) {
-      throw new \InvalidArgumentException('The method is not supported.');
+    if (substr($this->get('plugin_id'), 0, 7) === 'relaxed') {
+      $valid_methods = ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'HEAD'];
+      $normalised_method = strtoupper($method);
+      if (!in_array($normalised_method, $valid_methods)) {
+        throw new \InvalidArgumentException('The method is not supported.');
+      }
+      return $normalised_method;
+    } 
+    else {
+      parent::normalizeRestMethod($method);
     }
   }
 
