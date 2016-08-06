8cb11c0a2657365610f6bd84331591945f8dfcc6
jeqq <jeqq@1427908.no-reply.drupal.org>
2016-08-06 18:47:53 +0300

    Issue #2632370 by jeqq: Make tests pass on d.o again

--- src/HttpMultipart/ResourceMultipartResponse.php
+++ src/HttpMultipart/ResourceMultipartResponse.php
@@ -25,4 +25,32 @@ class ResourceMultipartResponse extends MultipartResponse implements CacheableRe
     return parent::prepare($request);
   }
 
+  /**
+   * Sends content for the current web response.
+   *
+   * @return Response
+   */
+  public function sendContent() {
+    // This fixes the "Malformed encoding found in chunked-encoding"
+    // error message in curl and makes possible to get the correct response body.
+    $size = $this->getSize();
+    echo "$size\r\n";
+    parent::sendContent();
+  }
+
+  /**
+   * Returns the length of all the parts in the response body.
+   *
+   * @return int
+   */
+  protected function getSize() {
+    $size = 0;
+    foreach ($this->parts as $part) {
+      $content = $part->getContent();
+      $output = "--{$this->boundary}" . "{$part->headers}" . $content;
+      $size += strlen($output);
+    }
+    return $size;
+  }
+
 }
