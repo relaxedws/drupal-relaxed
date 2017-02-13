490c714b785af7f1ce15e0cbf3ed90a21b3acea3
Andrei Jechiu <andrei.jechiu@gmail.com>
2017-02-13 15:14:49 +0200

    Fix response caching.

--- src/Plugin/rest/resource/ResourceBase.php
+++ src/Plugin/rest/resource/ResourceBase.php
@@ -81,8 +81,6 @@ abstract class ResourceBase extends CoreResourceBase implements RelaxedResourceI
           break;
 
         case 'GET':
-          // Restrict GET and HEAD requests to the media type specified in the
-          // _format query parameter.
           $collection->add("$route_name.$method", $route);
           break;
 
