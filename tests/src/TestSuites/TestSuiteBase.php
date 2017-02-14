1dd06d28ffb8c0aff18ae9e6a49f42c931f121e2
Andrei Jechiu <andrei.jechiu@gmail.com>
2017-02-14 10:17:55 +0200

    Try to fix integration tests

---
+++ tests/src/TestSuites/TestSuiteBase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\Tests\relaxed\TestSuites;
+
+//require_once __DIR__ . '/../../../../../core/tests/TestSuites/TestSuiteBase.php';
+// For TravisCI.
+require_once __DIR__ . '/../../../../../../www/core/tests/TestSuites/TestSuiteBase.php';
+
+use Drupal\simpletest\TestDiscovery;
+use Drupal\Tests\TestSuites\TestSuiteBase as CoreTestSuiteBase;
+
+/**
+ * Base class for Drupal test suites.
+ */
+abstract class TestSuiteBase extends CoreTestSuiteBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function addTestsBySuiteNamespace($root, $suite_namespace) {
+    if ($suite_namespace == 'Integration') {
+      $this->addTestFiles(TestDiscovery::scanDirectory("Drupal\\Tests\\", "$root/tests/src/$suite_namespace"));
+
+      $test_path = "$root/tests/src/$suite_namespace";
+      if (is_dir($test_path)) {
+        $this->addTestFiles(TestDiscovery::scanDirectory("Drupal\\Tests\\relaxed\\$suite_namespace\\", $test_path));
+      }
+    }
+  }
+
+}
