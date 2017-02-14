20496954474cdf7342c1a2acc964902b7fe41f35
Andrei Jechiu <andrei.jechiu@gmail.com>
2017-02-14 15:46:53 +0200

    Check if CouchDB is running.

--- tests/bin/replication.sh
+++ tests/bin/replication.sh
@@ -11,7 +11,6 @@ php ~/drush.phar --yes --uri=http://localhost:8081 site-install --sites-subdir=8
 php ~/drush.phar --yes --uri=http://localhost:8080 pm-enable entity_test, relaxed_test || true
 php ~/drush.phar --yes --uri=http://localhost:8081 pm-enable entity_test, relaxed_test || true
 
-# Check CouchDB installation.
-curl http://localhost:${COUCH_PORT}
+curl http://127.0.0.1:5984
 
-vendor/phpunit/phpunit/phpunit --verbose --debug --configuration phpunit.travis.xml --bootstrap core/tests/bootstrap.php
+vendor/phpunit/phpunit/phpunit --verbose --debug --configuration ~/www/phpunit.travis.xml --bootstrap ~/www/core/tests/bootstrap.php
