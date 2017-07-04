5d3998a135cbb8821cb7ad8e9b2a70cef62cf6fb
Damian Lee <damiankloip@gmail.com>
2017-07-04 13:28:35 +0100

    Moving current 2.x changes over to 1.x as a base

---
+++ src/Plugin/FormatNegotiatorManagerInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\relaxed\Plugin\FormatNegotiatorManagerInterface.
+ */
+
+namespace Drupal\relaxed\Plugin;
+
+/**
+ * Interface for format negotiator plugin manager.
+ */
+interface FormatNegotiatorManagerInterface {
+
+  /**
+   * Selects an appropriate negotiator
+   *
+   * @param string $format
+   * @param string $method
+   *
+   * @return \Drupal\relaxed\Plugin\FormatNegotiatorInterface|NULL
+   */
+  public function select($format, $method);
+
+}
