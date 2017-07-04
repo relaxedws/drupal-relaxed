457a363ee59b83ba62a3d014d2deefb2cd7bda6f
Damian Lee <damiankloip@gmail.com>
2017-07-04 14:06:44 +0100

    Add ApiResourceManager

---
+++ src/Annotation/ApiResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\relaxed\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines an API resource plugin annotation.
+ *
+ * @see \Drupal\relaxed\Plugin\FormatNegotiatorManager
+ * @see plugin_api
+ *
+ * @Annotation
+ */
+class ApiResource extends Plugin {
+
+  /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+
+  /**
+   * The label of the plugin.
+   *
+   * @var \Drupal\Core\Annotation\Translation
+   *
+   * @ingroup plugin_translatable
+   */
+  public $label;
+
+}
---
+++ src/Plugin/ApiResourceManagerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\relaxed\Plugin\ApiResourceManagerInterface.
+ */
+
+namespace Drupal\relaxed\Plugin;
+
+interface ApiResourceManagerInterface {
+
+}
