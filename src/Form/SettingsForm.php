dbcad3ebb1eed19fc9864d1a750498605c2f9463
Damian Lee <damiankloip@gmail.com>
2017-07-05 11:33:38 +0100

    Configurable authentication providers

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\relaxed\Form;
 
+use Drupal\Core\Authentication\AuthenticationCollectorInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class SettingsForm.
@@ -12,6 +15,30 @@ use Drupal\Core\Url;
  * @package Drupal\relaxed\Form
  */
 class SettingsForm extends ConfigFormBase {
+
+  protected $authenticationCollector;
+
+  /**
+   * Constructs a SettingsForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, AuthenticationCollectorInterface $collector) {
+    parent::__construct($config_factory);
+
+    $this->authenticationCollector = $collector;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('authentication_collector')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -39,6 +66,8 @@ class SettingsForm extends ConfigFormBase {
     if ($encrypt) {
       $encrypt_profiles = \Drupal::service('encrypt.encryption_profile.manager')->getEncryptionProfileNamesAsOptions();
     }
+    $authentication_providers = array_keys($this->authenticationCollector->getSortedProviders());
+    $authentication_providers = array_combine($authentication_providers, $authentication_providers);
 
     $form['api_root'] = [
       '#type' => 'textfield',
@@ -48,6 +77,14 @@ class SettingsForm extends ConfigFormBase {
       '#size' => 60,
       '#maxlength' => 128,
       '#required' => TRUE,
+    ];
+
+    $form['authentication'] = [
+      '#title' => $this->t('Authentication providers'),
+      '#description' => $this->t('The allowed authentication providers for relaxed routes.'),
+      '#type' => 'checkboxes',
+      '#options' => $authentication_providers,
+      '#default_value' => $config->get('authentication'),
     ];
 
     $form['creds'] = [
@@ -132,6 +169,10 @@ class SettingsForm extends ConfigFormBase {
     $encrypt = $form_state->getValue('encrypt');
     $encrypt_profile = $form_state->getValue('encrypt_profile');
 
+
+    if ($username && !($password || $config->get('password'))) {
+      $form_state->setErrorByName('password', 'When setting a username you must set a password.');
+    }
     if (!$username && $password) {
       $form_state->setErrorByName('username', 'A username must be entered when setting a password.');
     }
@@ -169,11 +210,14 @@ class SettingsForm extends ConfigFormBase {
 
     // Get configuration and update it from form.
     $config = $this->config('relaxed.settings');
+
     $config
       ->set('api_root', $form_state->getValue('api_root'))
+      ->set('authentication', array_keys(array_filter($form_state->getValue('authentication'))))
       ->set('username', $form_state->getValue('username'))
       ->set('encrypt', $form_state->getValue('encrypt'))
       ->set('encrypt_profile', $form_state->getValue('encrypt_profile'))
+      ->set('password', $form_state->getValue('password'))
       ->save();
 
     // If a password is entered then update it.
