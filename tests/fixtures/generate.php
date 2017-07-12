e9ac39994ccd53b9268e781b5e1c8b298ef0da68
Andrei Jechiu <andrei.jechiu@gmail.com>
2017-07-12 10:37:53 +0300

    Use @attachment key instead of _attachments.

--- tests/fixtures/generate.php
+++ tests/fixtures/generate.php
@@ -145,7 +145,7 @@ $docs = [
       'uri' => ['value' => 'public://file1.txt'],
       'filemime' => ['value' => 'text/plain'],
     ],
-    '_attachments' => _file_info(FILE1),
+    '@attachment' => _file_info(FILE1),
   ],
   // Entity with attachment.
   [
