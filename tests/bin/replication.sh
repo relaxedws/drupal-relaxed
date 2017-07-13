99d7b2c6697505b173978d0024337fc240367895
Andrei Jechiu <andrei.jechiu@gmail.com>
2017-07-13 14:53:24 +0300

    Testing.

--- tests/bin/replication.sh
+++ tests/bin/replication.sh
@@ -12,6 +12,6 @@ php ~/drush.phar --yes --uri=http://localhost:8080 pm-enable entity_test, relaxe
 php ~/drush.phar --yes --uri=http://localhost:8081 pm-enable entity_test, relaxed_test || true
 
 # Check CouchDB installation.
-curl http://localhost:${COUCH_PORT}
+curl http://localhost:5984
 
 vendor/phpunit/phpunit/phpunit --verbose --debug --configuration phpunit.travis.xml --bootstrap core/tests/bootstrap.php
