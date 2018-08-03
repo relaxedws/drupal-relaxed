b95e6f8bedfa582a5f5f04ec966324bebf983421
timmillwood <timmillwood@227849.no-reply.drupal.org>
2018-08-03 13:14:59 +0100

    Issue #2986257 by timmillwood, jeqq, l0ke: Clear cache after content replication

--- src/Plugin/rest/resource/EnsureFullCommitResource.php
+++ src/Plugin/rest/resource/EnsureFullCommitResource.php
@@ -2,11 +2,18 @@
 
 namespace Drupal\relaxed\Plugin\rest\resource;
 
+use Drupal\Core\Asset\AssetCollectionOptimizerInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\multiversion\Entity\WorkspaceInterface;
 use Drupal\rest\ModifiedResourceResponse;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
+ * Ensures the replication is complete.
+ *
  * This resource does not do anything since Drupal does not (yet) have a concept
  * of transactions across multiple requests. This resource only exists to comply
  * with the replication protocol.
@@ -25,9 +32,77 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 class EnsureFullCommitResource extends ResourceBase {
 
   /**
-   * @param $workspace
+   * The Module Handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The CSS Collection Optimizer.
+   *
+   * @var \Drupal\Core\Asset\AssetCollectionOptimizerInterface
+   */
+  protected $cssCollectionOptimizer;
+
+  /**
+   * The JS Collection Optimizer.
+   *
+   * @var \Drupal\Core\Asset\AssetCollectionOptimizerInterface
+   */
+  protected $jsCollectionOptimizer;
+
+  /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The Module Handler.
+   * @param \Drupal\Core\Asset\AssetCollectionOptimizerInterface $css_collection_optimizer
+   *   The CSS Collection Optimizer.
+   * @param \Drupal\Core\Asset\AssetCollectionOptimizerInterface $js_collection_optimizer
+   *   The JS Collection Optimizer.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, ModuleHandlerInterface $module_handler, AssetCollectionOptimizerInterface $css_collection_optimizer, AssetCollectionOptimizerInterface $js_collection_optimizer) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->moduleHandler = $module_handler;
+    $this->cssCollectionOptimizer = $css_collection_optimizer;
+    $this->jsCollectionOptimizer = $js_collection_optimizer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('module_handler'),
+      $container->get('asset.css.collection_optimizer'),
+      $container->get('asset.js.collection_optimizer')
+    );
+  }
+
+  /**
+   * Handle the post request.
+   *
+   * @param \Drupal\multiversion\Entity\WorkspaceInterface $workspace
+   *   The Workspace the replication is was.
    *
    * @return \Drupal\rest\ModifiedResourceResponse
+   *   The response.
    */
   public function post($workspace) {
     if (!$workspace instanceof WorkspaceInterface) {
@@ -39,6 +114,18 @@ class EnsureFullCommitResource extends ResourceBase {
       'instance_start_time' => (string) $workspace->getStartTime(),
     ];
 
+    // Flush all persistent caches.
+    // This is executed based on old/previously known information, which is
+    // sufficient, since new extensions cannot have any primed caches yet.
+    $this->moduleHandler->invokeAll('cache_flush');
+    foreach (Cache::getBins() as $service_id => $cache_backend) {
+      $cache_backend->deleteAll();
+    }
+
+    // Flush asset file caches.
+    $this->cssCollectionOptimizer->deleteAll();
+    $this->jsCollectionOptimizer->deleteAll();
+    _drupal_flush_css_js();
     return new ModifiedResourceResponse($response_data, 201);
   }
 
