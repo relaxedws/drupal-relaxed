62f4187ecda78a3850e67491a4248f39751539d2
Andrei Jechiu <andrei.jechiu@gmail.com>
2019-10-29 15:52:48 +0100

    More fixes.

--- tests/bin/run-couchdb.sh
+++ tests/bin/run-couchdb.sh
@@ -6,7 +6,7 @@ npm -g install npm@latest
 # Wait for couchdb to start, add CORS.
 npm install add-cors-to-couchdb
 while [ '200' != $(curl -s -o /dev/null -w %{http_code} http://127.0.0.1:${COUCH_PORT}) ]; do
-  echo waiting for couch to load... ;
+  echo "Waiting for couch to load...";
   sleep 1;
 done
 ./node_modules/.bin/add-cors-to-couchdb http://127.0.0.1:${COUCH_PORT}
